f6c6841f44971dc5342740ac35ab7ca451d3a3ca
JCanadasSabadell <e.jcanadasm@ies-sabadell.cat>
2025-02-20 15:36:27 +0100

    act 9 falta el 12 no se hacerlo :v

---
+++ Act9/Ex1.py
@@ -0,0 +1,7 @@
+lista = []
+lista.append(input("Introdueix un numero "))
+lista.append(input("Introdueix un numero "))
+lista.append(input("Introdueix un numero "))
+
+listao = lista.sort()
+print(listao)
